af046e0002d636d5e955b566e29f0f874b9de751
Luis Eduardo <luis10barbo2020@gmail.com>
2022-04-02 08:39:24 -0300

    Added topmes command

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import time
 import traceback
+from xmlrpc.client import Boolean
 import keep_alive
 import discord
 from discord.ext import tasks
 import get_token
 import logging
+import datetime
+import pickle
+import os
+import pytz
 
 # Logging
 logging.basicConfig(filename="discord.log", level=logging.DEBUG, format= "| {asctime} | {levelname:<8} > {message}", style="{",filemode="w")
@@ -19,31 +25,179 @@ def open_token():
       
 # Documentaton : https://discordpy.readthedocs.io/en/stable/search.html
 BOT_USER_ID = 864559656960524359
+TRACKER_BOT_ID_TEST = 901665260560322571
+TRACKER_BOT_ID_4FUN = 783428521753051136
+COMMAND_PREFIX = "!!"
 class MyClient(discord.Client):
+    
+    embed_files = {}
+    
     async def on_ready(self):
         print(f"Logged on as {self.user}")
         self.bot_user : discord.User = await self.fetch_user(BOT_USER_ID)
       
-    async def on_message(self, msg:discord.Message):
+    async def on_message(self, message:discord.Message):
         
         # Check messages
-        msg_lower = str(msg.content).lower()
-        print(f"Message from {msg.author}: {msg.content}")
+        msg_lower = str(message.content).lower()
+        print(f"Message from {message.author}: {message.content}")
         
-        if msg.author.id == self.bot_user.id:
+        if message.author.id == self.bot_user.id:
           pass
         else:
-            # Message at test channel
-            if msg.channel.id == 870640820364124162:
-                if "https://" in msg_lower or "http://" in msg_lower:
-                    await self.send_message(msg.channel.id, "Mandaram um link, ve se é skin <@!183743234084700160>")
+            # Commands
+            if msg_lower.startswith(COMMAND_PREFIX):
+                command_arguments = msg_lower[2:].split(" ")
+                print("Command Sent, command arguments -> ", command_arguments)
                 
-                    
+                # Tracker channel commands
+                TRACKER_CHANNEL = 770112076986712125
+                if message.channel.id == TRACKER_CHANNEL:
+                    # (prefix)topdomes
+                    if command_arguments[0] == "topmes":
+                        await self.top_do_mes(message)
+                     
+            # Message at test channel
+            elif message.channel.id == 870640820364124162:
+                if "https://" in msg_lower or "http://" in msg_lower:
+                    await self.send_message(message.channel.id, "Mandaram um link, ve se é skin <@!183743234084700160>")      
            
     # Use this Function with await, Ex: await send_message(id, msg) 
-    async def send_message(self, channel_id, msg):
-        channel = client.get_channel(channel_id)
-        await channel.send(msg)
+    async def send_message(self, channel_id, msg:str = None, embed:discord.Embed = None):
+        channel : discord.TextChannel = client.get_channel(channel_id)
+        await channel.send(content=msg, embed=embed)
+
+
+    # Functions
+    async def top_do_mes(self, message:discord.Message) -> list:
+        embed_file_name = f"embeds\\track_{message.channel.id}"
+        
+        def save_embed(embed:discord.Embed) -> None:
+            self.embed_files[message.channel.id] = {"created_at" : date_now,"embed_object": embed}
+            with open(embed_file_name, "wb") as file:
+                pickle.dump(self.embed_files[message.channel.id], file)
+                
+        def open_embed() -> Boolean:
+            # Check if file exists and creates one
+            if os.path.isfile(embed_file_name) == False:
+                return 1
+            
+            one_day_ago_date = date_now - datetime.timedelta(days=1)
+            
+            # Check if exists a embed file in class for channel id
+            
+            with open(embed_file_name, "rb") as file:
+                embed_dictionary = pickle.load(file)
+                created_at_timestamp = embed_dictionary["created_at"]
+                
+            print(created_at_timestamp, one_day_ago_date)
+                
+            if created_at_timestamp > one_day_ago_date:
+                return embed_dictionary["embed_object"]
+            else:
+                return 1
+                
+        async def get_bot_messages(day_limit=30, message_limit=None) -> list:        
+            latest_message_date = None
+            one_month_ago_date = date_now - datetime.timedelta(days=day_limit)
+            
+            bot_messages = []
+            
+            # Loop through messages till date limit
+            async for current_message in message.channel.history(limit=message_limit, after=one_month_ago_date):
+                latest_message_date = message.created_at
+                
+                # If date limit has already passed, break loop
+                if latest_message_date < one_month_ago_date:
+                    break
+                    
+                # If message is from bot, append to "bot_messages" list
+                if current_message.author.id == TRACKER_BOT_ID_TEST or current_message.author.id == TRACKER_BOT_ID_4FUN:
+                    if len(current_message.embeds) > 0:
+                        try:
+                            embed : discord.Embed = current_message.embeds[0]
+                            latest_message_date = current_message.created_at
+                        
+                            embed_title:str = embed.title
+                            embed_description:str = embed.description
+                            embed_author_name:str = embed.author.name
+                            
+                            parsed_message = {
+                                "player_name" : embed_author_name.rsplit("for ", 1)[1],
+                                "player_profile" : embed.author.url,
+                                "map_name" : embed_title.rsplit(" [", 1)[0],
+                                "map_link" : embed.url,
+                                "map_difficulty" : "[" + embed_title.rsplit(" [", 1)[1],
+                                "mods" : embed_description.split(" ", 1)[0],
+                                "pp" : embed_description.split("\n")[3].replace("*", "").split("|")[0].replace(" pp", ""),
+                                }
+                            
+                            bot_messages.append(parsed_message)
+                        except:
+                            traceback.print_exc()
+                        
+            # Sort by pp
+            bot_messages_sorted = sorted(bot_messages, key=lambda dictionary: dictionary["pp"], reverse=True)
+                    
+            return bot_messages_sorted
+        
+        def create_embed(bot_messages:dict, play_limit:int = 15) -> discord.Embed:
+            output_embed = discord.Embed()
+            
+            # Get output embed description
+            output_description = ""
+            for i in range(len(bot_messages)):
+                if i == play_limit:
+                    break
+                    
+                current_message = bot_messages[i]
+                
+                current_player = current_message["player_name"]
+                current_player_profile = current_message["player_profile"]
+                current_map = current_message["map_name"]
+                current_map_link = current_message["map_link"]
+                current_difficulty = current_message["map_difficulty"]
+                current_mods = current_message["mods"]
+                current_pp = current_message["pp"]
+                
+                current_entry = f"**[{current_player}]({current_player_profile})\n{current_pp}pp\n[{current_map} {current_difficulty}]({current_map_link})\n+{current_mods.replace('hrhd', 'hdhr').replace('dthd', 'hddt').replace('flhd', 'hdfl')}**"
+                output_embed.add_field(name=f"#{i + 1}", value=current_entry, inline=True)
+                # output_description = "\n\n".join([output_description, f"{current_entry}"])
+                
+            local_timezone = pytz.timezone("Brazil/East")
+            last_update_timestamp_tz = last_update_timestamp.replace(tzinfo=pytz.utc).astimezone(local_timezone) 
+            
+            output_embed.type = "rich"
+            output_embed.url = ""
+            output_embed.title = f"Top PP do Mês do canal #{message.channel.name}"
+            output_embed.description = f"OBS: Essas informações só podem ser atualizadas uma vez por dia!"
+            output_embed.set_footer(text=f"Última atualização: {last_update_timestamp_tz.day}/{last_update_timestamp_tz.month}/{last_update_timestamp_tz.year} às {last_update_timestamp_tz.hour}h{last_update_timestamp_tz.minute}m{last_update_timestamp_tz.second}s")
+            output_embed.colour = 4437377
+            output_embed.description = output_description
+                
+            return output_embed
+        
+        date_now = datetime.datetime.utcnow()
+        
+        result = open_embed()
+        if result == 1:
+            # Update list of pp plays
+            print("Updating top PP")
+            last_update_timestamp = date_now
+            
+            bot_messages = await get_bot_messages()
+
+            embed = create_embed(bot_messages=bot_messages)
+            save_embed(embed)
+        else:
+            
+            embed = result
+        print(result)
+        await self.send_message(channel_id=message.channel.id, msg=None, embed=embed)
+        
+        
+            
+            
 
 TOKEN = open_token()
 
